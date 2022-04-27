cf2f04413181d86a0520570650377abe97de356c
Taylor Yu <tlyu@mit.edu>
2022-04-27 12:21:54 -0500

    test WebGL trochoids

---
+++ assets/troch-webgl.js
@@ -0,0 +1,218 @@
+import * as THREE from 'three';
+import Stats from "https://unpkg.com/three@0.139/examples/jsm/libs/stats.module.js";
+import { EffectComposer } from 'https://unpkg.com/three@0.139/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.139/examples/jsm/postprocessing/RenderPass.js';
+import { AfterimagePass } from 'https://unpkg.com/three@0.139/examples/jsm/postprocessing/AfterimagePass.js';
+import { GUI } from 'https://unpkg.com/three@0.139/examples/jsm/libs/lil-gui.module.min.js';
+import { Line2 } from 'https://unpkg.com/three@0.139/examples/jsm/lines/Line2.js';
+import { LineGeometry } from 'https://unpkg.com/three@0.139/examples/jsm/lines/LineGeometry.js';
+import { LineMaterial } from 'https://unpkg.com/three@0.139/examples/jsm/lines/LineMaterial.js';
+
+const container = document.querySelector("div.gl-container");
+const canvas = document.querySelector("canvas.gl-container");
+let renderer;
+let stats;
+let camera;
+let scene;
+let composer;
+let afterimagePass;
+
+const nsamp = 1001;
+const verts = new Float32Array(nsamp * 3);
+
+let line;
+let material;
+
+function init_render() {
+    renderer = new THREE.WebGLRenderer({canvas});
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+    stats.dom.style.position = 'absolute'; // hack because it has no id or class name
+
+    const aspect = canvas.clientWidth/canvas.clientHeight;
+    // const camera = new THREE.PerspectiveCamera(60, aspect, 0.1, 100);
+    camera = new THREE.OrthographicCamera(
+        -1.1*aspect, 1.1*aspect, -1.1, 1.1, 0.1, 100);
+    camera.position.z = 2;
+
+    scene = new THREE.Scene();
+
+    composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+    afterimagePass = new AfterimagePass(0.8);
+    composer.addPass(afterimagePass);
+}
+
+function init_geometry() {
+    // const geometry = new THREE.BufferGeometry();
+    // geometry.setAttribute('position', new THREE.BufferAttribute(verts, 3));
+    // const material = new THREE.LineBasicMaterial({color: 0x3fcfff, linewidth: 2});
+    // const line = new THREE.Line(geometry, material);
+    const geometry = new LineGeometry();
+    geometry.setPositions(verts);
+    // worldUnits: true seems not to work with OrthographicCamera
+    material = new LineMaterial({
+        color: 0x3fcfff, linewidth: 0.005, worldUnits: false,
+        vertexColors: true, dashed: false, alphaToCoverage: true
+    });
+    const colors = [];
+    const color = new THREE.Color(0x3fcfff);
+    for (let i = 0; i < nsamp; i++) {
+        colors.push(color.r, color.g, color.b);
+    }
+    geometry.setColors(colors);
+    line = new Line2(geometry, material);
+    scene.add(line);
+}
+
+function resizeToDisplaySize() {
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+    let size = new THREE.Vector2;
+    renderer.getSize(size);
+    if (width != size.x
+        || height != size.y) {
+            const aspect = width/height;
+            camera.left = -1.1 * aspect;
+            camera.right= 1.1 * aspect;
+            renderer.setSize(width, height, false);
+            composer.setSize(width, height);
+            camera.updateProjectionMatrix();
+            console.log(width, height, size);
+        }
+}
+
+function toggleFillWindow() {
+    if (container.style.position != 'fixed') {
+        container.style.position = 'fixed';
+    } else {
+        container.style.position = 'relative';
+    }
+}
+
+class Osc {
+    constructor(f=1.0, ph=0.0) {
+        this.f = f;
+        this.ph = ph;
+    }
+    update() {
+        // Use fractional part to update phase, to avoid loss of precision
+        // at higher rotation frequencies
+        let ffrac = this.f % 1.0;
+         if (ffrac < 0) {
+             ffrac += 1.0;
+        }
+        this.ph += 2 * Math.PI * ffrac;
+        this.ph %= 2 * Math.PI;
+    }
+    x(val) {
+        return Math.cos(2 * Math.PI * this.f * val + this.ph);
+    }
+    y(val) {
+        return Math.sin(2 * Math.PI * this.f * val + this.ph);
+    }
+    v(val) {
+        const th = 2 * Math.PI * this.f * val + this.ph;
+        return [Math.cos(th), Math.sin(th)];
+    }
+}
+
+class Troch {
+    constructor(a=-3, b=1, bend=0.01, fr=0.001) {
+        this._a = a;
+        this._b = b;
+        this._bend = bend;
+
+        this.osc_a = new Osc();
+        this.osc_b = new Osc();
+        this.osc_r = new Osc(fr);
+        this.setf();
+    }
+    setf() {
+        // Recalculate oscillator frequencies
+        const k = math.fraction(this._a, this._b);
+        this.osc_a.f = k.d;
+        // Adjust bend to keep constant rotation with k.d changes
+        this.osc_b.f = k.d - k.s*k.n + this._bend/k.d;
+    }
+    v(val) {
+        const [xa, ya] = this.osc_a.v(val);
+        const [xb, yb] = this.osc_b.v(val);
+        const r = 1 - this.osc_r.x(val) ** 2;
+        return [r * xa + (1-r) * xb, r * ya + (1-r) * yb];
+    }
+    update() {
+        this.osc_a.update();
+        this.osc_b.update();
+        this.osc_r.update();
+    }
+    get a() { return this._a; }
+    set a(val) {
+        this._a = val;
+        this.setf();
+    }
+    get b() { return this._b; }
+    set b(val) {
+        this._b = val;
+        this.setf();
+    }
+    get bend() { return this._bend; }
+    set bend(val) {
+        this._bend = val;
+        this.setf();
+    }
+    get fa() { return this.osc_a.f; }
+    get fb() { return this.osc_b.f; }
+}
+
+function init_gui() {
+    const gui = new GUI({container});
+    const agui = gui.addFolder('Animation');
+    agui.add(troch, 'bend', 0, 0.1, 0.001).name('spin');
+    agui.add(troch.osc_b, 'ph', 0, 2*Math.PI, 0.001)
+        .name('spin ph.').listen();
+    agui.add(troch.osc_r, 'f', -0.01, 0.01, 0.001).name('breathe');
+    agui.add(troch.osc_r, 'ph', 0, 2*Math.PI, 0.0001)
+        .name('breathe ph.').listen();
+
+    const cgui = gui.addFolder('Curve');
+    cgui.add(troch, 'a', -13, 13, 1);
+    cgui.add(troch, 'b', 1, 11, 1);
+    cgui.add(troch, 'fa').listen();
+    cgui.add(troch, 'fb').listen();
+    cgui.close();
+
+    const rendergui = gui.addFolder('Rendering');
+    rendergui.add(afterimagePass.uniforms['damp'], 'value', 0, 1, 0.01)
+        .name('trails');
+    rendergui.add(material, 'linewidth', 0.001, 0.02, 0.001);
+    rendergui.close();
+    gui.add(gui, 'reset');
+    gui.add({'toggle expand': toggleFillWindow}, 'toggle expand');
+}
+
+function animate() {
+    resizeToDisplaySize();
+    for (let i = 0; i < nsamp; i++) {
+        const t = i/(nsamp - 1);
+        const [x, y] = troch.v(t);
+        verts[3*i] = x;
+        verts[3*i+1] = y;
+        verts[3*i+2] = 0.0;
+    }
+    troch.update();
+    line.geometry.setPositions(verts);
+    // line.geometry.attributes.position.needsUpdate = true;
+    composer.render(scene, camera);
+    stats.update();
+    requestAnimationFrame(animate);
+}
+
+const troch = new Troch();
+init_render();
+init_geometry();
+init_gui();
+requestAnimationFrame(animate);
---
+++ troch-test.md
@@ -0,0 +1,41 @@
+---
+title: More Test WebGL
+katex: true
+---
+
+Interactive [WebGL](https://en.wikipedia.org/wiki/WebGL) demo of [hypotrochoids](https://en.wikipedia.org/wiki/Hypotrochoid).
+Uses [three.js](https://threejs.org/).
+
+$$
+z = r \exp(i \theta) + (1-r) \exp\left({b-a\over b} i \theta\right)
+$$
+
+<style>
+    .lil-gui { --name-width: 25%; }
+    .lil-gui.root { position: absolute; top: 0px; right: 0px; --width: 200px; }
+    div.gl-container { position: relative; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 9000; }
+    canvas.gl-container { display: block; min-height: 400px; width: 100%; }
+</style>
+<div class="gl-container">
+    <canvas class="gl-container" style="display: block"></canvas>
+</div>
+<script async src="https://unpkg.com/es-module-shims@1.5.4/dist/es-module-shims.js"></script>
+<script type="importmap">
+  {
+    "imports": {
+      "three": "https://unpkg.com/three@0.139/build/three.module.js"
+    }
+  }
+</script>
+<script src="https://unpkg.com/mathjs@10.5.0/lib/browser/math.js"></script>
+<script type="module" src="/assets/troch-webgl.js"></script>
+
+## Details
+
+$a$ and $b$ set the "gearing" of the circles (their rotation speeds).
+The "breathing" is cosine-squared modulation of $r$ (relative radii of the circles).
+The "spin" (not shown in the equation) tweaks the rotation speeds to be slightly off from the gearing.
+
+Try changing the curve parameters.
+You might notice that the curve complexity jumps around a bit when $a$ and $b$ have a common factor.
+(Their ratio is what's important, not their actual values.)
